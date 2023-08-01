89b256062ccefea91b527c6297e92d3d13465702
Alco2 <abdou.lahoub.wwe@gmail.com>
2023-08-01 22:55:15 +0100

    edit test

--- tests/test_app.py
+++ tests/test_app.py
@@ -12,20 +12,3 @@ def client():
 def test_homepage(client):
     response = client.get('/')
     assert response.status_code == 200
-
-def test_getOneName(client):
-    response = client.get('/api/randomname')
-    assert response.status_code == 200
-
-def test_getMultipleName(client):
-    response = client.get('/api/randomname/5')
-    assert response.status_code == 200
-
-def test_getAllNames(client):
-    response = client.get('/api/names')
-    assert response.status_code == 200
-
-def test_getARandomName(client):
-    response = client.get('/api/names/1')
-    assert response.status_code == 200
-
