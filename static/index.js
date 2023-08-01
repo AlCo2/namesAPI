5b103ec956aad2dfe6b5cbad9222c56f0c983abc
Alco2 <abdou.lahoub.wwe@gmail.com>
2023-08-01 02:05:53 +0100

    add javascript and styles

--- static/index.js
+++ static/index.js
@@ -1,7 +1,10 @@
 let openNavBtn = document.getElementById("openNav");
 let sidebar = document.getElementById("sidebar");
-
+let closeNavBtn = document.getElementById("closeNav");
 
 openNavBtn.onclick = () =>{
     sidebar.style.transform = "translateX(0)";
+}
+closeNavBtn.onclick = () =>{
+    sidebar.style.transform = "translateX(-100%)";
 }
