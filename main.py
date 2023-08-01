4b4ff93825bb88880d205bb2bee58b0b53ed59b4
Alco2 <abdou.lahoub.wwe@gmail.com>
2023-08-01 18:00:46 +0100

    add test

--- main.py
+++ main.py
@@ -3,7 +3,99 @@ import mysql.connector
 import json
 import random
 from db import DATABASE_CONFIG
-app = Flask(__name__)
+
+
+def create_app():
+    app = Flask(__name__)
+    @app.route('/', methods=['GET', 'POST'])
+    def welcome():
+        name = 'name'
+        if request.method == 'POST':
+            gender = request.form['gender']
+            region = request.form.getlist('region')
+            if(len(region)>0):
+                name = get_randome_name(gender, region[0])
+            else:
+                region = random.randrange(1,4)
+                name = get_randome_name(gender, region)
+        return render_template('index.html', name=name)
+
+
+    @app.route("/api/names", methods=['GET'])
+    def get_names():
+        connection = get_mysql_connection()
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM names")
+        data = cursor.fetchall()
+        result = formatToJson(data)
+        response = Response(
+            response=json.dumps(result),
+            status=200,
+            mimetype='application/json'
+        )
+        cursor.close()
+        if(result):
+            return response
+        return jsonify({"message": "error in fetching data"}), 404
+
+
+    @app.route("/api/names/<int:id>")
+    def get_name(id):
+        connection = get_mysql_connection()
+        cursor = connection.cursor()
+        cursor.execute(f"SELECT * FROM names where id={id}")
+        data = cursor.fetchall()
+        result = formatToJson(data)
+        response = Response(
+            response=json.dumps(result),
+            status=200,
+            mimetype='application/json'
+        )
+        cursor.close()
+        if(result):
+            return response
+        return jsonify({"message": "error in fetching data"}), 404
+
+    @app.route('/api/randomname')
+    def get_random_data():
+        connection = get_mysql_connection()
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM names")
+        random_data = cursor.fetchall()
+        result = formatToJson(random_data)
+        result = random.choices(result)
+        response = Response(
+            response=json.dumps(result),
+            status=200,
+            mimetype='application/json'
+        )
+        cursor.close()
+        if(result):
+            return response
+        return jsonify({"message": "error in fetching data"}), 404
+
+    @app.route('/api/randomname/<int:id>')
+    def get_random_multiple_data(id):
+        if(id>50):
+            id = 50
+        if(id<=0):
+            id = 1
+        connection = get_mysql_connection()
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM names")
+        random_data = cursor.fetchall()
+        result = formatToJson(random_data)
+        result = random.choices(result, k=id)
+        response = Response(
+            response=json.dumps(result),
+            status=200,
+            mimetype='application/json'
+        )
+        cursor.close()
+        if(result):
+            return response
+        return jsonify({"message": "error in fetching data"}), 404
+    return app
 
 
 def get_mysql_connection():
@@ -31,95 +123,6 @@ def get_randome_name(gender, region):
     result = random.choices(random_data)
     return result[0][1]
 
-@app.route('/', methods=['GET', 'POST'])
-def welcome():
-    name = 'name'
-    if request.method == 'POST':
-        gender = request.form['gender']
-        region = request.form.getlist('region')
-        if(len(region)>0):
-            name = get_randome_name(gender, region[0])
-        else:
-            region = random.randrange(1,4)
-            name = get_randome_name(gender, region)
-    return render_template('index.html', name=name)
-
-
-
-@app.route("/api/names", methods=['GET'])
-def get_names():
-    connection = get_mysql_connection()
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM names")
-    data = cursor.fetchall()
-    result = formatToJson(data)
-    response = Response(
-        response=json.dumps(result),
-        status=200,
-        mimetype='application/json'
-    )
-    cursor.close()
-    if(result):
-        return response
-    return jsonify({"message": "error in fetching data"}), 404
-
-
-@app.route("/api/names/<int:id>")
-def get_name(id):
-    connection = get_mysql_connection()
-    cursor = connection.cursor()
-    cursor.execute(f"SELECT * FROM names where id={id}")
-    data = cursor.fetchall()
-    result = formatToJson(data)
-    response = Response(
-        response=json.dumps(result),
-        status=200,
-        mimetype='application/json'
-    )
-    cursor.close()
-    if(result):
-        return response
-    return jsonify({"message": "error in fetching data"}), 404
-
-@app.route('/api/randomname')
-def get_random_data():
-    connection = get_mysql_connection()
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM names")
-    random_data = cursor.fetchall()
-    result = formatToJson(random_data)
-    result = random.choices(result)
-    response = Response(
-        response=json.dumps(result),
-        status=200,
-        mimetype='application/json'
-    )
-    cursor.close()
-    if(result):
-        return response
-    return jsonify({"message": "error in fetching data"}), 404
-
-@app.route('/api/randomname/<int:id>')
-def get_random_multiple_data(id):
-    if(id>50):
-        id = 50
-    if(id<=0):
-        id = 1
-    connection = get_mysql_connection()
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM names")
-    random_data = cursor.fetchall()
-    result = formatToJson(random_data)
-    result = random.choices(result, k=id)
-    response = Response(
-        response=json.dumps(result),
-        status=200,
-        mimetype='application/json'
-    )
-    cursor.close()
-    if(result):
-        return response
-    return jsonify({"message": "error in fetching data"}), 404
-
-
-app.run()
+if __name__ == '__main__':
+    app = create_app()
+    app.run()
---
+++ tests/test_app.py
@@ -0,0 +1,20 @@
+from main import create_app
+import pytest
+
+@pytest.fixture
+def client():
+    app = create_app()
+    app.config['TESTING'] = True
+    client = app.test_client()
+    yield client
+
+
+def test_homepage(client):
+    response = client.get('/')
+    assert response.status_code == 200
+
+def test_api(client):
+    response = client.get('/api/randomname')
+    assert response.status_code == 200
+    response = client.get('/api/randomname/5')
+    assert response.status_code == 200
