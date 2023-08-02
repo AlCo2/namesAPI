e7df6e99f7263134305aa1ad5b46dcd4baddd199
Alco2 <abdou.lahoub.wwe@gmail.com>
2023-08-02 13:56:11 +0100

    add responsive design

--- main.py
+++ main.py
@@ -10,98 +10,96 @@ DATABASE_CONFIG = {
     'database': 'nameapi'
 }
 
-def create_app():
-    app = Flask(__name__)
-    @app.route('/', methods=['GET', 'POST'])
-    def welcome():
-        name = 'name'
-        if request.method == 'POST':
-            gender = request.form['gender']
-            region = request.form.getlist('region')
-            if(len(region)>0):
-                name = get_randome_name(gender, region[0])
-            else:
-                region = random.randrange(1,4)
-                name = get_randome_name(gender, region)
-        return render_template('index.html', name=name)
+app = Flask(__name__)
+@app.route('/', methods=['GET', 'POST'])
+def welcome():
+    name = 'name'
+    if request.method == 'POST':
+        gender = request.form['gender']
+        region = request.form.getlist('region')
+        if(len(region)>0):
+            name = get_randome_name(gender, region[0])
+        else:
+            region = random.randrange(1,4)
+            name = get_randome_name(gender, region)
+    return render_template('index.html', name=name)
 
 
-    @app.route("/api/names", methods=['GET'])
-    def get_names():
-        connection = get_mysql_connection()
-        cursor = connection.cursor()
-        cursor.execute("SELECT * FROM names")
-        data = cursor.fetchall()
-        result = formatToJson(data)
-        response = Response(
-            response=json.dumps(result),
-            status=200,
-            mimetype='application/json'
-        )
-        cursor.close()
-        if(result):
-            return response
-        return jsonify({"message": "error in fetching data"}), 404
+@app.route("/api/names", methods=['GET'])
+def get_names():
+    connection = get_mysql_connection()
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM names")
+    data = cursor.fetchall()
+    result = formatToJson(data)
+    response = Response(
+        response=json.dumps(result),
+        status=200,
+        mimetype='application/json'
+    )
+    cursor.close()
+    if(result):
+        return response
+    return jsonify({"message": "error in fetching data"}), 404
 
 
-    @app.route("/api/names/<int:id>")
-    def get_name(id):
-        connection = get_mysql_connection()
-        cursor = connection.cursor()
-        cursor.execute(f"SELECT * FROM names where id={id}")
-        data = cursor.fetchall()
-        result = formatToJson(data)
-        response = Response(
-            response=json.dumps(result),
-            status=200,
-            mimetype='application/json'
-        )
-        cursor.close()
-        if(result):
-            return response
-        return jsonify({"message": "error in fetching data"}), 404
+@app.route("/api/names/<int:id>")
+def get_name(id):
+    connection = get_mysql_connection()
+    cursor = connection.cursor()
+    cursor.execute(f"SELECT * FROM names where id={id}")
+    data = cursor.fetchall()
+    result = formatToJson(data)
+    response = Response(
+        response=json.dumps(result),
+        status=200,
+        mimetype='application/json'
+    )
+    cursor.close()
+    if(result):
+        return response
+    return jsonify({"message": "error in fetching data"}), 404
 
-    @app.route('/api/randomname')
-    def get_random_data():
-        connection = get_mysql_connection()
-        cursor = connection.cursor()
-        cursor.execute("SELECT * FROM names")
-        random_data = cursor.fetchall()
-        result = formatToJson(random_data)
-        result = random.choices(result)
-        response = Response(
-            response=json.dumps(result),
-            status=200,
-            mimetype='application/json'
-        )
-        cursor.close()
-        if(result):
-            return response
-        return jsonify({"message": "error in fetching data"}), 404
+@app.route('/api/randomname')
+def get_random_data():
+    connection = get_mysql_connection()
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM names")
+    random_data = cursor.fetchall()
+    result = formatToJson(random_data)
+    result = random.choices(result)
+    response = Response(
+        response=json.dumps(result),
+        status=200,
+        mimetype='application/json'
+    )
+    cursor.close()
+    if(result):
+        return response
+    return jsonify({"message": "error in fetching data"}), 404
 
-    @app.route('/api/randomname/<int:id>')
-    def get_random_multiple_data(id):
-        if(id>50):
-            id = 50
-        if(id<=0):
-            id = 1
-        connection = get_mysql_connection()
-        cursor = connection.cursor()
-        cursor.execute("SELECT * FROM names")
-        random_data = cursor.fetchall()
-        result = formatToJson(random_data)
-        result = random.choices(result, k=id)
-        response = Response(
-            response=json.dumps(result),
-            status=200,
-            mimetype='application/json'
-        )
-        cursor.close()
-        if(result):
-            return response
-        return jsonify({"message": "error in fetching data"}), 404
-    return app
-
+@app.route('/api/randomname/<int:id>')
+def get_random_multiple_data(id):
+    if(id>50):
+        id = 50
+    if(id<=0):
+        id = 1
+    connection = get_mysql_connection()
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM names")
+    random_data = cursor.fetchall()
+    result = formatToJson(random_data)
+    result = random.choices(result, k=id)
+    response = Response(
+        response=json.dumps(result),
+        status=200,
+        mimetype='application/json'
+    )
+    cursor.close()
+    if(result):
+        return response
+    return jsonify({"message": "error in fetching data"}), 404
+    
 
 def get_mysql_connection():
     connection = mysql.connector.connect(**DATABASE_CONFIG)
@@ -129,5 +127,4 @@ def get_randome_name(gender, region):
     return result[0][1]
 
 if __name__ == '__main__':
-    app = create_app()
     app.run()
